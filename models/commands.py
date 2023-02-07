82119d80867930155436147933d490e04994398e
Marc Rosenberger <marc.rosenberger100@gmail.com>
2023-02-07 22:34:17 +0100

    ServerIp command added

--- models/commands.py
+++ models/commands.py
@@ -1,4 +1,5 @@
 from discord import Message
+import urllib.request
 
 
 class Command:
@@ -8,3 +9,12 @@ class Command:
     def get_permissions(self):
         pass
 
+
+class ServerIp(Command):
+    async def run(self, message: Message):
+        ip = urllib.request.urlopen('https://v4.ident.me/').read().decode('utf8')
+        await message.channel.send(ip)
+
+    def get_permissions(self):
+        return
+    
